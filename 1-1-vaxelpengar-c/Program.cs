7f4371888e70aa37169d516cec539faee66d56d6
Caroline Forslund <info@carolineforslund.se>
2014-10-08 14:54:44 +0200

    Started project at level C

---
+++ 1-1-vaxelpengar-c/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Resources;
+
+namespace _1_1_vaxelpengar_c
+{
+    class Program
+    {
+        //public virtual string GetString(
+        //        string Strings
+        //    );
+
+        static void Main(string[] args)
+        {
+            uint subtotal;
+
+            subtotal = (uint)ReadPositiveDouble("Skriv in summa:"); 
+            ReadUint("Erhållet belopp:", subtotal); 
+        }
+
+        static double ReadPositiveDouble(string prompt)
+        {
+         double subtotal;
+
+         Console.Write(prompt);
+         subtotal = double.Parse(Console.ReadLine());
+         return subtotal; 
+        }
+
+        static uint ReadUint(string prompt, uint minValue)
+        {
+            uint recievedMoney;
+
+            Console.Write(prompt);
+            recievedMoney = uint.Parse(Console.ReadLine());
+            return recievedMoney;
+        }
+
+        static uint[] SplitIntoDenominations(uint change, uint[] denominations)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void ViewMessage(string message) //static void ViewMessage(string message [bool isError = false])
+        {
+        }
+
+        static void ViewReciept(double roundingOffAmount, uint total, uint cash, uint change, uint[] notes, uint[] denominations)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
